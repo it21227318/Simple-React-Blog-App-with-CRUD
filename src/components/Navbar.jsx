c27604c2c79b98aec30328a202200befb607c53f
Denuka Jayasekara <it21227318@my.sliit.lk>
2024-02-09 14:08:39 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1>Blog</h1>
+        <div className="links">
+            <a href="#">Home</a>
+            <a href="#">New</a>
+        </div>
+    </nav>
+  )
+}
